7fbccda7d72c62ee72a81e69a4c85f3300f3ee0e
Ilham Ibrahim <farrel.ahmad.zaidan@gmail.com>
2023-04-17 10:04:42 +0700

    New landing page project for intern web

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./node_modules/flowbite/**/*.js"
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
