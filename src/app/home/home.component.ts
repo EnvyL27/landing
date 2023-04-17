16eb27af2df9235ded65d3261cd1a22b5586025f
Ilham Ibrahim <farrel.ahmad.zaidan@gmail.com>
2023-04-17 14:49:26 +0700

    add simple parallaxx

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit() {
+
+  }
+
+  parallax(){
+    let langit = document.getElementById('langit');
+    let pohon = document.getElementById('pohon');
+    let card = document.getElementById('card');
+
+  window.addEventListener('scroll', function(){
+    // let.value = window.scrollY;
+    // langit.style.left = value * 0,25 + 'px';
+  })
+  }
+
+
+
 
 }
